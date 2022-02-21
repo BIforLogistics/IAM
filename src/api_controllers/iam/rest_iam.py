c2fcf4d6d0ab1a411b434f3ad176e180479817d4
Kharanshu <kharanshu.b@rightrev.com>
2022-02-21 16:11:20 +0530

    Added schema for db

--- src/api_controllers/iam/rest_iam.py
+++ src/api_controllers/iam/rest_iam.py
@@ -1,6 +1,12 @@
 import logging
 from flask import Blueprint, Flask, Response, jsonify, request
+from lib.models import db
+from configs.flask_config import APP
 
 ROUTE = Blueprint('ROUTE',__name__)
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+
+@APP.before_first_request
+def create_table():
+    db.create_all()
