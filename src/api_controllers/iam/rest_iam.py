432c25e05acc8263bd91f65e48fd30150f02d0f8
hariram <hariram.rajagopalan@gmail.com>
2022-07-09 15:16:27 +0530

    added login api

--- src/api_controllers/iam/rest_iam.py
+++ src/api_controllers/iam/rest_iam.py
@@ -39,6 +39,23 @@ def get_user_by_id(id):
         resp = jsonify({"message": "Internal server error"})
         resp.status_code = 500   
         return resp
+@ROUTE.route('/user', methods=['POST'])
+def user_login():
+    try:
+        request_data = request.get_json()
+        user_name = request_data["id"]
+        password = request_data["passwd"]
+        from libs.models import User_Model
+        return_value = User_Model.get_user_by_id(user_name)
+        if password == return_value["passwd"]:
+        resp = jsonify({"message": "Successfully logged in"})
+        resp.status_code = 200   
+        return resp
+    except Exception as e:
+        logging.info(f"{e}")
+        resp = jsonify({"message": "Internal server error"})
+        resp.status_code = 500   
+        return resp
 
 @ROUTE.route('/user', methods=['POST'])
 def add_user():
